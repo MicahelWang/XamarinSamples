5f6e7f733a34d088d0df6fc83531ab68143fce2a
Michael  Wang <ydgz-wt@163.com>
2015-10-16 13:27:46 +0800

    update

--- HubsDemo/Hubs1/MainActivity.cs
+++ HubsDemo/Hubs1/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -8,10 +9,10 @@ using Android.OS;
 
 namespace Hubs1
 {
-    [Activity(Label = "Hubs1", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "@string/MainTitle", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        int _count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -24,8 +25,30 @@ namespace Hubs1
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", _count++); };
+
+
+        }
+
+
+
+        private void BindDataView()
+        {
+
+            ////SimpleExpandableListAdapter(Context context, IList<IDictionary<string, object>> groupData, 
+            ////int expandedGroupLayout, 
+            ////int collapsedGroupLayout, 
+            ////string[] groupFrom, int[] groupTo, IList<IList<IDictionary<string, object>>> childData, 
+            ////int childLayout, int lastChildLayout, string[] childFrom, int[] childTo);
+            //var groupData = new List<Dictionary<string, object>>();
+            //var expandedGroupLayout = 1;
+            //var collapsedGroupLayout = 1;
+            //string[] groupFrom=new string[] {};
+            //int[] groupTo=new int[] {};
+
+            //IExpandableListAdapter adapter = new SimpleExpandableListAdapter(this, data,);
         }
     }
+
 }
 
