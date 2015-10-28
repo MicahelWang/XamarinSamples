4f541a03cf116ec2c3cea29c2e45abc4dd7843f9
Michael  Wang <ydgz-wt@163.com>
2015-10-28 15:55:56 +0800

    udpate

---
+++ MvvmHubs1/Hubs1.Core/App.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Core;
+using Cirrious.MvvmCross.ViewModels;
+using Hubs1.Core.ViewModels;
+
+namespace Hubs1.Core
+{
+    public interface IErrorReporter
+    {
+        void ReportError(string error);
+    }
+
+    public class ErrorEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public ErrorEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+
+    public interface IErrorSource
+    {
+        event EventHandler<ErrorEventArgs> ErrorReported;
+    }
+
+    public class ErrorApplicationObject
+        : MvxMainThreadDispatchingObject
+        , IErrorReporter
+        , IErrorSource
+    {
+        public void ReportError(string error)
+        {
+            if (ErrorReported == null)
+                return;
+
+            InvokeOnMainThread(() =>
+            {
+                var handler = ErrorReported;
+                handler?.Invoke(this, new ErrorEventArgs(error));
+            });
+        }
+
+        public event EventHandler<ErrorEventArgs> ErrorReported;
+    }
+
+    public class App
+        : MvxApplication
+    {
+        public App()
+        {
+            RegisterAppStart<HotelListViewModel>();
+
+            var errorApplicationObject = new ErrorApplicationObject();
+            Mvx.RegisterSingleton<IErrorReporter>(errorApplicationObject);
+            Mvx.RegisterSingleton<IErrorSource>(errorApplicationObject);
+        }
+    }
+}
---
+++ MvvmHubs1/Hubs1.Core/ViewModels/HotelDataViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace Hubs1.Core.ViewModels
+{
+    public class HotelDataViewModel : BaseViewModel
+    {
+        private double R = 6371229;
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+        public double Distance { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {3}km({1}, {2})", Name, Longitude.ToString("0.0000"), Latitude.ToString("0.0000"), Distance.ToString("0.00"));
+        }
+
+        public void GetDistance(double longt1, double lat1)
+        {
+            double x, y;
+            x = (Longitude - longt1) * Math.PI * R
+              * Math.Cos(((lat1 + Latitude) / 2) * Math.PI / 180) / 180;
+            y = (Latitude - lat1) * Math.PI * R / 180;
+
+            Distance = (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) / 1000);
+        }
+
+        public ICommand ShowCategoryCommand
+        {
+            get { return new MvxCommand(() => ShowViewModel<HotelViewModel>(this)); }
+        }
+    }
+}
